de6d314fce093315c451c829cd1d7a6620e19642
javithalion <javier_cervera_cano@hotmail.com>
2015-05-31 13:35:51 +0200

    Domain created (POCO objects) Still pending to add calc methods in order and order line

---
+++ TryCatch.WebShopCase.Domain/Base/Entity.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TryCatch.WebShopCase.Domain.Base
+{
+    public class Entity<T>
+    {
+        /// <summary>
+        /// Gets or sets the identifier for this entity
+        /// </summary>
+        public T Id { get; set; }
+    }
+}
---
+++ TryCatch.WebShopCase.Domain/Base/TrackedEntity.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TryCatch.WebShopCase.Domain.Base
+{
+    public class TrackedEntity<T> : Entity<T>
+    {
+        /// <summary>
+        /// Gets or sets when this entyty was created
+        /// </summary>
+        public DateTime CreationDate { get; set; }
+    }
+}
